751ce275dd774616f43d70efc4d830d30e855617
Ashwini <076bct012.ashwini@pcampus.edu.np>
2023-07-10 19:00:50 +0545

    modified for deployment

--- portfolio/src/components/Projects.tsx
+++ portfolio/src/components/Projects.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Card from "./Card";
 import NN_front from "../../public/assets/NN_frontend.png";
+import Image from "next/image";
 
 const Projects = () => {
   return (
@@ -10,12 +10,26 @@ const Projects = () => {
     >
       <div className="text-4xl font-bold text-slate-700">PROJECTS</div>
       <div className="mt-4 mb-8 flex flex-col items-center">
-        <Card
-          image_url={NN_front}
-          title="Novel Navigator"
-          desc="WebApp for personalized book recommendations. Created Using React and Flask."
-          project_link="https://github.com/Ashwini4869/Novel-Navigator-vite"
-        />
+        <div className="bg-white p-4 w-1/2 text-slate-700 font-sans border-emerald-500 border-2 rounded-md">
+          <Image
+            width={500}
+            height={500}
+            src={NN_front}
+            alt="Project_Cover"
+            className="rounded-md"
+          ></Image>
+          <h1 className="font-semibold mt-2 pt-2 pl-2">Novel Navigator</h1>
+          <h1 className="p-2">
+            WebApp for personalized book recommendations. Developed using React
+            and Flask.
+          </h1>
+          <a
+            href="https://github.com/Ashwini4869/Novel-Navigator-vite"
+            className="mt-1 px-3 py-1 hover:bg-emerald-600 hover:text-white rounded-full mb-2"
+          >
+            View on GitHub
+          </a>
+        </div>
       </div>
     </div>
   );
